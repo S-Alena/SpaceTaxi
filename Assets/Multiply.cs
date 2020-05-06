b9eecc45f8021d7d53d08e8bef6d40e82340c570
Rob Impoli <microsoft@impoli.eu>
2020-05-06 21:21:48 +0200

    I hope this works now

---
+++ Assets/Multiply.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+public class Multiply : MonoBehaviour
+{
+    public Transform prefab;
+
+    void Start()
+    { 
+        CreateInstances();
+    }
+
+    public Transform[] yourPrefabVariations;
+    public int instanceCount = 10;
+    public Vector2 hey = new Vector2(100,100);
+     
+    
+    private List<Transform> _instances = new List<Transform>();
+ 
+    private void CreateInstances() {
+        for (int i = 0; i < instanceCount; ++i) {
+            // Randomly pick prefab variation:
+            var yourPrefab = yourPrefabVariations[ Random.Range(0, yourPrefabVariations.Length) ];
+            var instance = Instantiate(yourPrefab) as Transform;
+            _instances.Add(instance);
+ 
+            // Then adjust your position as needed.
+            instance.localPosition = hey;
+            hey += hey; 
+        }
+    }
+    }
+
+    
