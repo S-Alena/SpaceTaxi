3a5e2cdf343089ea60b77f8bb516f85a66d24b5b
Angela <angela.scherle@web.de>
2020-05-13 11:26:57 +0200

    Added Script that wasn't added yet

---
+++ Assets/PassengerCount.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PassengerCount : MonoBehaviour
+{
+    public int redPassengerCount = 0;
+    public int greenPassengerCount = 0;
+    public int yellowPassengerCount = 0;
+
+    public Text redPassengerDisplay;
+    public Text greenPassengerDisplay;
+    public Text yellowPassengerDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameEvents.current.onPassengerPickup += UpdatePassengerCount;
+    }
+
+    private void UpdatePassengerCount()
+    {
+        redPassengerCount = redPassengerCount + 1;
+        TextChange();
+    }
+
+    public void TextChange()
+    {
+        redPassengerDisplay.text = "Passengers: " + redPassengerCount;
+    }
+}
