ef9d57265f9cdd212626af7255073f6886d3c9b7
S-Alena <56514418+S-Alena@users.noreply.github.com>
2020-05-26 16:14:09 +0200

    Scripts commited.

---
+++ Assets/Scenes/Scripts/Size.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Size : MonoBehaviour
+{
+    //public Renderer renderer;
+    internal static Vector3 mSize;
+    
+    void Start()
+    {
+        mSize = GetComponent<EdgeCollider2D>().bounds.size/2;
+        //print("IDK Mate: " + mSize);
+        
+    }
+
+
+    void Update()
+    {
+        
+
+    }
+}
