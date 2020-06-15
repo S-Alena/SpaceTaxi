9f356812e136ca7d56676dfe21f95233bd8140be
S-Alena <56514418+S-Alena@users.noreply.github.com>
2020-06-15 17:57:51 +0200

    added files

---
+++ Assets/UIManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Button StartGame;
+    public Button QuitGame;
+    public string level;
+
+    void Start()
+    {
+        Button btn = StartGame.GetComponent<Button>();
+        btn.onClick.AddListener(LoadLevel);
+        
+        Button btn2 = QuitGame.GetComponent<Button>();
+        btn2.onClick.AddListener(Quit);
+    }
+
+    void Update()
+    {
+    }
+
+
+    
+    public void LoadLevel()
+    {
+        Application.LoadLevel(level);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
