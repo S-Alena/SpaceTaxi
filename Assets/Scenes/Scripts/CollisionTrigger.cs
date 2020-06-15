a2d2758d81257902c9295ea9c3bed7c443619b9d
Angela <angela.scherle@web.de>
2020-06-15 15:24:59 +0200

    Assets added

---
+++ Assets/Scenes/Scripts/CollisionTrigger.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+
+public class CollisionTrigger : MonoBehaviour
+{
+    public Text endText;
+    public bool end = false;
+
+    void Start()
+    {
+        endText.enabled = false;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Debug.Log("Collided with " + collision.gameObject.name);
+
+
+        Color red = new Color(1, 0, 0, 1);
+        Color green = new Color(0, 1, 0, 1);
+        Color blue = new Color(0, 0, 1, 1);
+        Color yellow = new Color(1, 1, 0, 1);
+        int passengerCount = 0;
+
+        if (collision.gameObject.GetComponent<SpriteRenderer>().color == red)
+        {
+            passengerCount = PassengerCount.redPassengerCount;
+        }
+        else if (collision.gameObject.GetComponent<SpriteRenderer>().color == green)
+        {
+            passengerCount = PassengerCount.greenPassengerCount;
+        }
+        else if (collision.gameObject.GetComponent<SpriteRenderer>().color == blue)
+        {
+            passengerCount = PassengerCount.bluePassengerCount;
+        }
+        else if (collision.gameObject.GetComponent<SpriteRenderer>().color == yellow)
+        {
+            passengerCount = PassengerCount.yellowPassengerCount;
+        }
+        else if (collision.gameObject.name == "Home")
+        {
+
+            if (PassengerCount.yellowPassengerCount == 0 && PassengerCount.greenPassengerCount == 0 && PassengerCount.bluePassengerCount == 0 && PassengerCount.redPassengerCount == 0)
+            {
+                endText.text = "You made it. " + Environment.NewLine + "Transported = " + PassengerCount.transported;
+            }
+            else
+            {
+                endText.text = "Game Over." + Environment.NewLine + "Press to Restart";
+            }
+            endText.enabled = true;
+            end = true;
+
+        }
+
+        GameEvents.current.PlanetCollision(collision.gameObject.name, passengerCount);
+    }
+
+}
