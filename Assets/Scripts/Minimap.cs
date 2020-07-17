14267e3553084cc04ca0552eec9e669a442265c8
Angela <angela.scherle@web.de>
2020-07-17 15:17:02 +0200

    Minimap added

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    private bool isShowing = true;
+    private Canvas map;
+    // Start is called before the first frame update
+    void Start()
+    {
+        map = this.GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("m"))
+        {
+            isShowing = !isShowing;
+            map.enabled = isShowing;
+        }
+    }
+}
