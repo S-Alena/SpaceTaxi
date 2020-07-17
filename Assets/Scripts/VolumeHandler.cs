476adfdaef227df8c1311622481b7b2550028442
Bexy-Lyn <bexy_lyn@outlook.de>
2020-07-17 17:15:06 +0200

    Volume-Regler für Levels ergänzt, funktioniert noch nicht global.

---
+++ Assets/Scripts/VolumeHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeHandler : MonoBehaviour
+{
+    public Button volumeToggle;
+    public GameObject volumeSlider;
+    public AudioSource bgMusic, sfx;
+    Slider slider;
+
+    private float value = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeSlider.SetActive(false);
+        bgMusic.volume = value;
+        sfx.volume = value + 0.2f;
+
+        slider = volumeSlider.GetComponent<Slider>();
+        slider.value = value;
+
+        Button volumeB = volumeToggle.GetComponent<Button>();
+        volumeB.onClick.AddListener(ToggleVolumeSlider);
+    }
+
+    public void Update()
+    {
+        value = slider.value;
+        bgMusic.volume = value;
+        if(value + 0.2f <= 1)
+        {
+            sfx.volume = value + 0.2f;
+        }
+        else
+        {
+            sfx.volume = 1;
+        }
+    }
+
+    public void ToggleVolumeSlider()
+    {
+        if (volumeSlider.active == true)
+        {
+            volumeSlider.SetActive(false);
+        }
+        else
+        {
+            volumeSlider.SetActive(true);
+        }
+    }
+
+}
