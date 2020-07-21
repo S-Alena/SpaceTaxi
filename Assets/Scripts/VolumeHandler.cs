7b8a61fb8ed5ed0eb4df15cae6db4a6e87e3a20b
qlRitscher <ritscher@skouz.de>
2020-07-21 15:51:38 +0200

    Volume Fade^

--- Assets/Scripts/VolumeHandler.cs
+++ Assets/Scripts/VolumeHandler.cs
@@ -4,43 +4,56 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.UI;
-
 using Debug = UnityEngine.Debug;
 
 public class VolumeHandler : MonoBehaviour
 {
     private static readonly string FirstPlay = "FirstPlay";
     private static readonly string VolumePref = "VolumePref";
-    private int firstPlayInt;
     private float volumeFloat;
 
+    public float fadeDuration;
     public Button volumeToggle;
     public GameObject volumeSlider;
     public AudioSource bgMusic;
     public AudioSource[] sfx;
     Slider slider;
+    IEnumerator volumeCoroutine;
+    public float targetVolume { get; private set; }
 
-    // Start is called before the first frame update
     void Start()
     {
         volumeSlider.SetActive(false);
         Button volumeB = volumeToggle.GetComponent<Button>();
         volumeB.onClick.AddListener(ToggleVolumeSlider);
 
+        volumeFloat = PlayerPrefs.GetFloat(VolumePref, 0.3f);
         slider = volumeSlider.GetComponent<Slider>();
-
-        slider = volumeSlider.GetComponent<Slider>();
-        volumeFloat = PlayerPrefs.GetFloat(VolumePref);
         slider.value = volumeFloat;
-        Debug.Log("More Play detected, setting volumeFloat:" + volumeFloat);
+        targetVolume = volumeFloat;
 
+        bgMusic.volume = 0f;
+        VolumeTransition(targetVolume, fadeDuration, bgMusic);
+        for (int i = 0; i < sfx.Length; i++)
+        {
+            sfx[i].volume = 0f;
+            if (targetVolume + 0.2f <= 1)
+            {
+                VolumeTransition(targetVolume + 0.2f, fadeDuration, sfx[i]);
+            }
+            else
+            {
+                VolumeTransition(1f, fadeDuration, sfx[i]);
+            }
+        }
     }
-    
+
     public void Update()
     {
         slider = volumeSlider.GetComponent<Slider>();
+
         bgMusic.volume = slider.value;
-        for(int i = 0; i < sfx.Length; i++)
+        for (int i = 0; i < sfx.Length; i++)
         {
             if (slider.value + 0.2f <= 1)
             {
@@ -77,6 +90,52 @@ public class VolumeHandler : MonoBehaviour
         if (!inFocus)
         {
             SaveSoundSettings();
+        }
+    }
+
+    IEnumerator VolumeTransitionCoroutine(float target, float duration, AudioSource audio)
+    {
+        /*Debug.Log("VolumeTransitionCoroutine aufgerufen.");
+        float start = audio.volume;
+        float lastTime = 1;
+        while (start < target)
+        {
+            if (Time.time - lastTime > 0.1f)
+            {
+                audio.volume += (target - start) / duration / 60;
+                lastTime = Time.time;
+            }
+
+        }
+        yield return null;*/
+        bgMusic.volume = 0f;
+        float progress = 0.0f;
+        while (progress < 1.0f && bgMusic.volume < targetVolume)
+        {
+        progress += Time.deltaTime / duration;
+        Debug.Log("progress: " + progress);
+        bgMusic.volume += progress;
+        Debug.Log("volume: " + bgMusic.volume);
+        yield return null;
+        }
+    }
+
+    public void VolumeTransition(float target, float duration, AudioSource audio)
+    {
+        Debug.Log("VolumeTransition aufgerufen.");
+        targetVolume = target;
+        if (volumeCoroutine != null)
+        {
+            StopCoroutine(volumeCoroutine);
+        }
+        if (duration <= Mathf.Epsilon && duration > 0)
+        {
+            audio.volume = target;
+        }
+        else
+        {
+            volumeCoroutine = VolumeTransitionCoroutine(target, duration, audio);
+            StartCoroutine(volumeCoroutine);
         }
     }
 }
